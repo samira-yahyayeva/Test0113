fb105e34882c587d6ef188a38244f94f9c2a3b7a
samira-yahyayeva <64815404+samira-yahyayeva@users.noreply.github.com>
2025-01-13 20:33:39 -0500

    Add files via upload

---
+++ YouTubeTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import config.DriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import pages.YouTubePage;
+import utils.Constants;
+
+public class YouTubeTest {
+    private static YouTubePage youTubePage;
+
+    @BeforeAll
+    public static void setUp() {
+        DriverManager.getDriver().get(Constants.BASE_URL);
+        youTubePage = new YouTubePage(DriverManager.getDriver());
+    }
+
+    @Test
+    public void testPlayMusic() {
+        youTubePage.search("Relaxing Music");
+        youTubePage.playFirstVideo();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        DriverManager.quitDriver();
+    }
+}
